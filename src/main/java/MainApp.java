cf4650f9a331999205a2fe74c1ae4aa6a7679338
Benjamin-Bergman <git@gdane.net>
2024-05-20 13:10:35 -0400

    Update code style

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,30 +1,32 @@
+/*
+ * Copyright (c) Benjamin Bergman 2024.
+ */
+
 import java.awt.*;
 
-public class MainApp
-{
-    public static void main(String[] args)
-    {
+@SuppressWarnings("UtilityClass")
+final class MainApp {
+    @SuppressWarnings("FeatureEnvy")
+    public static void main(String[] args) {
         // This starter code to get you familiar with how
         // the TurtleLogo application works
 
         // The world is your canvas
-        World world = new World(200, 200);
-        Turtle turtle = new Turtle(world,-100, -100);
-        
         int width = 200;
         int height = 200;
+        World world = new World(width, height);
+        Turtle turtle = new Turtle(world, -100, -100);
 
         // calculate the hypotenuse (diagonal)
         // a2 + b2 = c2
-        double widthSquared = Math.pow(width, 2);
-        double heightSquared = Math.pow(height, 2);
-        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
-
 
         turtle.setPenWidth(3);
         turtle.setColor(Color.GREEN);
 
         turtle.turnRight(45);
+        double widthSquared = Math.pow(width, 2);
+        double heightSquared = Math.pow(height, 2);
+        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
         turtle.forward(hypotenuse);
 
         turtle.penUp();
